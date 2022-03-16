f9489ce2fab287db5fc69682f57557765fddaa49
LucasVidigal98 <lucasvidigal3@gmail.com>
2022-03-16 20:38:31 -0300

    Created SignIn Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,55 @@
+import { Button, Flex, FormControl, FormLabel, Input, Stack } from '@chakra-ui/react';
+
 
 export default function Home() {
   return (
-    <h1>Teste</h1>
+    <Flex 
+      w='100vw'
+      h="100vh" 
+      align="center" 
+      justify="center"
+    >
+      <Flex 
+        as="form" 
+        width="100%" 
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        alignItems="center"
+        justify="center"
+        flexDirection="column"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <Input 
+              name="email" 
+              type="email" 
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant='filled'
+              _hover={ { bg: 'gray.900' } }
+              size="lg"
+            />
+          </FormControl>
+          
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+            <Input 
+              name="password" 
+              type="password" 
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant='filled'
+              _hover={ { bg: 'gray.900' } }
+              size="lg"
+            />  
+            </FormControl>
+        </Stack>
+
+        <Button type="submit" mt="6" width="90%" size="lg" colorScheme={'pink'}>Entrar</Button>
+      </Flex>
+    </Flex>
   )
 }
