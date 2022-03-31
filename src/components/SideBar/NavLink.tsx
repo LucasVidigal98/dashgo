b5ef48b336d4917471b20c9eb76f374ae6f16bed
LucasVidigal98 <lucasvidigal3@gmail.com>
2022-03-30 21:57:14 -0300

    Added Link routes

--- src/components/SideBar/NavLink.tsx
+++ src/components/SideBar/NavLink.tsx
@@ -1,16 +1,20 @@
-import { Icon, Link, Text, LinkProps } from '@chakra-ui/react';
+import { Icon, Link as ChakraLink, Text, LinkProps } from '@chakra-ui/react';
+import Link from 'next/link';
 import React, { ElementType } from 'react'
 
 interface NavLinkProps extends LinkProps {
   icon: ElementType;
   children: string;
+  href: string;
 }
 
-function NavLink({ icon, children, ...rest }: NavLinkProps) {
+function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
   return (
-    <Link display="flex" alignItems="center" {...rest}>
-      <Icon as={icon} />
-      <Text ml="4" fontWeight="medium">{children}</Text>
+    <Link href={href} passHref>
+      <ChakraLink display="flex" alignItems="center" {...rest}>
+        <Icon as={icon} />
+        <Text ml="4" fontWeight="medium">{children}</Text>
+      </ChakraLink>
     </Link>
   )
 }
