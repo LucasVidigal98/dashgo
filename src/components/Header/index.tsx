c7449e6325fb131ba311dfcedb64b08d3cf90853
LucasVidigal98 <lucasvidigal3@gmail.com>
2022-03-30 21:35:34 -0300

    Responsive Sidebar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
+import { RiMenuLine } from "react-icons/ri";
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
 import Logo from "./Logo";
 import NotificationNav from "./NotificationNav";
 import Profile from "./Profile";
 import SearchBox from "./SearchBox";
 
 function Header() {
+  const { onOpen } = useSidebarDrawer();
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -21,6 +25,20 @@ function Header() {
       px="6"
       align="center"
     >
+
+      { !isWideVersion && (
+        <IconButton 
+          icon={<Icon as={RiMenuLine} />} 
+          aria-label="Open Navigation"
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        >
+        </IconButton>
+      )}
+
+
       <Logo />
 
       { isWideVersion && <SearchBox /> }
