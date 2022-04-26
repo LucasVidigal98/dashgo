40abaf6ebb2823dccdfba6abf5285c606903df0a
LucasVidigal98 <lucasvidigal3@gmail.com>
2022-04-25 21:34:13 -0300

    Added Mirajs And react-query to the project

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,13 +1,34 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue } from '@chakra-ui/react';
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue, Spinner } from '@chakra-ui/react';
 import Link from 'next/link';
-
-import React from 'react'
+import React, { useEffect } from 'react'
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
+import { useQuery } from 'react-query';
+
 import Header from '../../components/Header';
 import Pagination from '../../components/Pagination';
 import SideBar from '../../components/SideBar';
 
 function UserList() {
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users');
+    const data = await response.json();
+
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        }),
+      }
+    })
+
+    return users;
+  });
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -19,7 +40,7 @@ function UserList() {
 
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <SideBar />
-  
+
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">Usuários</Heading>
@@ -29,59 +50,76 @@ function UserList() {
                 size="sm"
                 fontSize="sm"
                 colorScheme="pink"
-                leftIcon={<Icon as={RiAddLine} fontSize="20"/>}
+                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
               >
                 Criar novo
               </Button>
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuários</Th>
-                { isWideVersion &&  <Th>Data de cadastro</Th> }
-                <Th width="8"></Th>
-              </Tr>
-            </Thead>
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex>
+              <Text>Falha ao obter dados dos usuários.</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuários</Th>
+                    {isWideVersion && <Th>Data de cadastro</Th>}
+                    <Th width="8"></Th>
+                  </Tr>
+                </Thead>
 
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
+                <Tbody>
+                  {data.map(user => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px={["4", "4", "6"]}>
+                          <Checkbox colorScheme="pink" />
+                        </Td>
 
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Lucas F. Vidigal</Text>
-                    <Text fontSize="sm" color="gray.300">lucasvidigal3@gmail.com</Text>
-                  </Box>
-                </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold">{user.name}</Text>
+                            <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                          </Box>
+                        </Td>
 
-                { isWideVersion && <Td>29 de março de 2022</Td> }
+                        {isWideVersion && <Td>{user.createdAt}</Td>}
 
-                { isWideVersion &&(
-                  <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      Editar
-                    </Button>
-                  </Td>
-                )}
-                
-              </Tr>
-            </Tbody>
-          </Table>
+                        {isWideVersion && (
+                          <Td>
+                            <Button
+                              as="a"
+                              size="sm"
+                              fontSize="sm"
+                              colorScheme="purple"
+                              leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                            >
+                              Editar
+                            </Button>
+                          </Td>
+                        )}
 
-          <Pagination />
+                      </Tr>
+                    )
+                  })}
+                </Tbody>
+              </Table>
+
+              <Pagination />
+            </>
+          )}
+
         </Box>
       </Flex>
     </Box>
---
+++ src/services/migrage/index.ts
@@ -0,0 +1,47 @@
+import { createServer, Factory, Model } from 'miragejs';
+import * as faker from '@faker-js/faker';
+
+type User = {
+  name: string;
+  email: string;
+  created_at: string;
+}
+
+export function makeServer() {
+  const server = createServer({
+    models: {
+      user: Model.extend<Partial<User>>({})
+    },
+
+    factories: {
+      user: Factory.extend({
+        name(i: number) {
+          return `User ${i}`;
+        },
+        email() {
+          return faker.faker.internet.email().toLowerCase();
+        },
+        createdAt() {
+          return faker.faker.date.recent(10);
+        }
+      })
+    },
+
+    seeds(server) {
+      server.createList('user', 10);
+    },
+
+    routes() {
+      this.namespace = 'api';
+      this.timing = 750;
+
+      this.get('/users');
+      this.post('/users');
+
+      this.namespace = '';
+      this.passthrough();
+    }
+  });
+
+  return server;
+}
